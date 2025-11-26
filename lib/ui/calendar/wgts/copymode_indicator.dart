27a2277a77b7a4f8c76cdf064855c54eadfd6060
David F <dfitzsimmons11@gmail.com>
2025-11-25 23:54:10 -0500

    comment update- copymode indicator deprecated

--- lib/ui/calendar/wgts/copymode_indicator.dart
+++ lib/ui/calendar/wgts/copymode_indicator.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timeshare/data/providers/cal/cal_providers.dart';
 import 'package:timeshare/data/enums.dart';
 
+// DEPRECATED
 class CopyModeIndicator extends ConsumerWidget {
   const CopyModeIndicator({super.key});
 
@@ -10,13 +11,13 @@ class CopyModeIndicator extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final mode = ref.watch(interactionModeStateProvider);
     final copiedEvent = ref.watch(copyEventStateProvider);
-    
+
     if (mode != InteractionMode.copy) return const SizedBox.shrink();
-    
+
     String str = '';
     if (copiedEvent != null) str = copiedEvent.name.trim();
     if (str.length > 10) str = str.substring(0, 9) + ('...');
-    
+
     return FilledButton(
       onPressed: () {
         ref.read(interactionModeStateProvider.notifier).setNormal();
